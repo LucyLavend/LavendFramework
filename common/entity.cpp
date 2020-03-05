a0c4d4d38c039593c7ec5dc1a264ee78935f30c7
LucyLavend <jongebloedlucy@gmail.com>
2020-03-05 11:42:21 +0100

    Add start for entity class

---
+++ common/entity.cpp
@@ -0,0 +1,11 @@
+#include <common/entity.h>
+
+Entity::Entity()
+{
+	
+}
+
+Entity::~Entity()
+{
+	
+}
---
+++ common/entity.h
@@ -0,0 +1,14 @@
+#ifndef ENTITY_H
+#define ENTITY_H
+
+class Entity
+{
+public:
+	Entity();
+	virtual ~Entity();
+
+private:
+	
+};
+
+#endif
